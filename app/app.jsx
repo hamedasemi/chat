3310485d0124347d89fbaba1d703973251e0f79f
hamedasemi <h-@live.se>
2020-06-12 15:59:37 +0200

    Clean

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import style from './app.css'
+
 import { Store } from './components/store/store';
 import Products from './components/products/products';
 import Cart from './components/cart/cart';
@@ -9,11 +11,13 @@ import Search from './components/search/search';
 const App = () => {
 
     return (
-        <Store>
-            <Search></Search>
-            <Products></Products>
-            <Cart></Cart>
-        </Store>
+        <div className={style.app}>
+            <Store>
+                <Search></Search>
+                <Products></Products>
+                <Cart></Cart>
+            </Store>
+        </div>
     )
 }
 
